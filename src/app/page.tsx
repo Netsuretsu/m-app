c756a8f94e34d3702bc5d0373b653e524bfbb9a5
Netsuretsu <constantin-joval.awanchede@epitech.eu>
2025-01-17 01:10:54 +0100

    added login and register forms component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function LandingPage() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      Welcome to M-app
+    </div>
+  );
+}
